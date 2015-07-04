a2f1e3234734f149018c6d6c852a753eccbdbde0
Casey <Casey.Kriutzfield@live.com>
2015-07-04 12:05:39 -0700

    Updated consttructor to accept a path to where the assets are.

--- Sharpku/Generator.cs
+++ Sharpku/Generator.cs
@@ -6,18 +6,29 @@ namespace Sharpku
 {
     public class Generator
     {
-
+        private readonly string _path;
         List<string> _list5 = new List<string>();
         List<string> _list7 = new List<string>();
         List<string> _name1 = new List<string>();
         List<string> _name2 = new List<string>();
 
+        public Generator()
+        {
+            _path = @"Assets\";
+        }
+
+        public Generator(string filePathForLists)
+        {
+            _path = filePathForLists;
+        }
+
         public string GenerateHaiku()
         {
-            _list5 = ReadListOfSentences(@"Assets\list5");
-            _list7 = ReadListOfSentences(@"Assets\list7");
-            _name1 = ReadListOfSentences(@"Assets\name1");
-            _name2 = ReadListOfSentences(@"Assets\name2");
+            //_list5 = ReadListOfSentences(@"Assets\list5");
+            _list5 = ReadListOfSentences(_path + "list5");
+            _list7 = ReadListOfSentences(_path + "list7");
+            _name1 = ReadListOfSentences(_path + "name1");
+            _name2 = ReadListOfSentences(_path + "name2");
 
             var rnd = new Random();
 
